30414302bc57bbff49319067aac477f0e701330e
Avinash <74913043+AvinashWarrior@users.noreply.github.com>
2022-12-21 19:41:16 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+source1 = "C:/Users/user/Downloads"
+destinationforsource = "C:/Users/user/Downloads/Testing"
+listFiles = os.listdir(source1)
+for filename in listFiles:
+    name,extension = os.path.splitext(filename)
+    if extension == '':
+        continue
+    if extension in ['.gif','.png','.jpeg','.jpg','.heic','.jfif']:
+        p1 = source1+'/'+filename
+        p2 = destinationforsource+'/'+"Images"
+        p3 = destinationforsource+'/'+"Images"+'/'+filename
+        if os.path.exists(p2):
+            shutil.move(p1,p3)
+        else:
+            os.makedirs(p2)
+            shutil.move(p1,p3)
+
+
